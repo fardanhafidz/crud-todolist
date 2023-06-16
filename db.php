3c42f5b84f5f3fd4f92636b9ac4a0b9d58978307
fardanhafidz <muhfardanhafidz@gmail.com>
2023-06-16 13:45:10 +0700

    first commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$host_name = 'localhost';
+$db_user = 'ayyubdza_ayyub';
+$db_pass = 'kartawirana1981';
+$db_name = 'ayyubdza_ayyub';
+
+$con = mysqli_connect($host_name,$db_user,$db_pass,$db_name);
+
+?>
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+require 'functions.php';
+if(isset($_POST['todo'])){
+    createTodo($_POST);
+}
+
+?>
+
+
+<!doctype html>
+<html lang="id">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.rtl.min.css" integrity="sha384-WJUUqfoMmnfkBLne5uxXj+na/c7sesSJ32gI7GfCk4zO4GthUKhSEGyvQ839BC51" crossorigin="anonymous">
+
+    <title>Todo List</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 m-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>My Todolist</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="index.php" method="post">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1">Todo</label>
+                                <input type="text" class="form-control" name="todo" placeholder="Enter your Task">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1">Todo Time</label>
+                                <input type="date" class="form-control" name="todo_time">
+                            </div>
+                            <button type="submit" name="todo_submit" class="btn btn-primary">Submit</button>
+                        </form>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
